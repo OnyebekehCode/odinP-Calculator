9450e3845d313de885ec0a362bb777864096d69f
Samuel Onyebekeh <deemedx0@gmail.com>
2025-09-21 16:38:23 +0100

    upload html and css files

---
+++ app.js
@@ -0,0 +1,48 @@
+const buttons = document.getElementsByClassName("buttons")
+const display = document.getElementsByClassName("screen-button")
+const clearbtn = document.getElementsByClassName("clear-button")
+//Add and display result once two numbers are calculated(continue if more numbers are added)
+
+ /*function add(a, b, c){
+    
+    if(c === "+"){
+    return a + b
+    }else { return "Error"
+}
+}
+console.log(add(2, 4 , "+"))
+
+
+
+
+//substract and display result once two numbers are calculated(continue if more numbers are added)
+
+function subtract(a, b, c){
+    if(c === "-"){
+    return a - b
+    }else { return "Error"
+}
+}
+console.log(subtract(2,4, "-"))
+
+
+//Divide and display result once two numbers are calculated(continue if more numbers are added)
+
+function divide(a, b, c){
+    if(c === "/"){
+    return a / b
+    }else { return "Error"
+}}
+console.log(divide(2,4, "/"))
+
+
+
+//multiply and display result once two numbers are calculated(continue if more numbers are added)
+
+function multiply(a, b, c){
+    if(c === "*"){
+    return a * b
+    }else { return "Error"
+}
+}
+console.log(multiply(2,4, "*"))*/
