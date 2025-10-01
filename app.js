0a31598d371725b2121f2fd4cfc79a32421420cb
Samuel Onyebekeh <deemedx0@gmail.com>
2025-10-01 14:41:45 +0100

    remove all exc. eventlisteners for numbers and buttons

--- app.js
+++ app.js
@@ -1,72 +1,70 @@
 //get all elements needed
 const buttons = document.getElementsByClassName("buttons")
 const operbuttons= document.getElementsByClassName("oper-buttons")
-const equalbutton = document.getElementsByClassName("equalbutton")
-const display = document.getElementsByClassName("screen-button")
+const equalbutton = document.getElementsByClassName("equalbutton")[0]
+const display = document.getElementsByClassName("screen-button")[0]
 const clearbtn = document.getElementById("clear-button")
 
-//asign variables neccessary
-let numbers;
-let operator;
-let numArr = [];
-let displayScreen = [];
+let operator = [];
+let firstNumArr = [];
+let secondNumArr = [];
+let calcArr = [];
 
 
-//button to listen for different click events
 
-//listen for click on buttons for textcontent containing only numbers 
-for (let i = 0; i < buttons.length; i++) {
- buttons[i].addEventListener("click", function(){
-        numArr.push(buttons[i].textContent)
-    return console.log(numArr)
-})    
-}
-//listen for click on buttons for text content containing only operators
 for (let i = 0; i < operbuttons.length; i++) {
- operbuttons[i].addEventListener("click", function(){
-        numArr.push(operbuttons[i].textContent)
-    return console.log(numArr)
-})    
-}
-//clear the array holding inputs
- clearbtn.addEventListener("click", function(){
-      numArr.splice(0, numArr.length)
-    return console.log(numArr)})
-//test if the two buttons works together and as expected
+    operbuttons[i].addEventListener("click", function(){
+       operator.push(operbuttons[i].textContent);   
+       return operator.join("")
+    }
+)
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+       firstNumArr.push(buttons[i].textContent);
+       return firstNumArr.join("")
+    })}
+
+    for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        if (operator != null && firstNumArr != null) {
+         secondNumArr.push(buttons[i].textContent);
+        }
+       return secondNumArr.join("")  
+    })}}
+
 setTimeout(() => {
-    console.log(numArr)
-}, 10000); 
-// test calculation using values in the array
-let seperatedVal = numArr.split("")
-console.log(seperatedVal)
+     console.log(calcArr) 
+}, 8000);   
+if (firstNumArr != null) {
+       calcArr.push(firstNumArr, operator) 
+}else if
+(operator != null && firstNumArr != null) {
+        calcArr.push(firstNumArr, operator, secondNumArr);
+    }else
+        console.log("relax, you are there yet")
 
-//neccessary functions
-
+setTimeout(() => {
+     console.log(calcArr) 
+}, 8000);   
 //add function
- function add(a, b, c){
-    
-    if(c === "+"){
+ function add(a, b){
     return a + b
-    }else { return "Error"
 }
-}
+
 //subtract function
-function subtract(a, b, c){
-    if(c === "-"){
+function subtract(a, b){
     return a - b
-    }else { return "Error"
-}
 }
 //division function
-function divide(a, b, c){
-    if(c === "/"){
+function divide(a, b){
     return a / b
-    }else { return "Error"
-}}
+}
 //multiplication function 
-function multiply(a, b, c){
-    if(c === "*"){
+function multiply(a, b){
     return a * b
-    }else { return "Error"
 }
-}
+
+
+equalbutton.addEventListener("click", function() {
+    calcArr(numArr);
+});
