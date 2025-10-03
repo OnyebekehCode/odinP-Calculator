291179f83c7745aaa2e8a1dd440647b9f5a91d36
Samuel Onyebekeh <deemedx0@gmail.com>
2025-10-03 12:22:58 +0100

    more js updates

--- app.js
+++ app.js
@@ -8,36 +8,61 @@ const clearbtn = document.getElementById("clear-button")
 let operator = [];
 let firstNumArr = [];
 let secondNumArr = [];
-let calcArr = [];
+let result = []
 
 for (let i = 0; i < operbuttons.length; i++) {
     operbuttons[i].addEventListener("click", function(){
-       operator.push(operbuttons[i].textContent);   
-       return operator.join("")
-    }
-)
+       operator.push(operbuttons[i].textContent);
+       display.textContent = operator.join("")
+   
+       return console.log(operator.join(""))
+    })}
+
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
        firstNumArr.push(buttons[i].textContent);
-       return firstNumArr.join("")
+       display.textContent = firstNumArr.join("")
+       return console.log(firstNumArr.join(""))
     })}
 
     for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
         if (operator != null && firstNumArr != null) {
          secondNumArr.push(buttons[i].textContent);
+         display.textContent = secondNumArr.join("")
         }
-       return secondNumArr.join("")  
-    })}}
+       return console.log(secondNumArr.join(""))
+    })}
+
+    clearbtn.addEventListener("click", function(){
+        firstNumArr = [];
+        secondNumArr = [];
+        operator = [];
+        display.textContent = "|"
+    }    )
+
+
+    equalbutton.addEventListener("click", function(){
+       display.textContent = calcArr()
+
+    })
   
-if (firstNumArr != null) {
-       calcArr.push(firstNumArr, operator) 
-}else if
-(operator != null && firstNumArr != null) {
-        calcArr.push(firstNumArr, operator, secondNumArr);
-    }else
-        console.log("relax, you are there yet")
- 
+function calcArr (firstNumArr, secondNumArr){
+        if (firstNumArr != null && secondNumArr  != null && operator  != null) {
+    switch (operator) {
+        case "+": result.push(add(firstNumArr,secondNumArr))
+             break;
+        case "-": result.push(subtract(firstNumArr,secondNumArr))
+            break;
+        case "/": divide(firstNumArr,secondNumArr)
+             break;  
+        case "*": multiply(firstNumArr,secondNumArr)
+             break;    
+    }
+    return
+    }
+}
+
 //add function
  function add(a, b){
     return a + b
