1ea9990301e0cfff273476f8b74dc4af8071bbce
Samuel Onyebekeh <deemedx0@gmail.com>
2025-10-14 15:16:59 +0100

    update ui

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 let firstNum;
 let secNum;
 let operator;
+let numbers = document.getElementsByClassName("buttons");
+let operNumbers = document.getElementsByClassName("oper-buttons")
+let calculate = document.getElementById("equalbutton")
+let clear = document.getElementById("clear-button")
+display = document.getElementById("screen-button")
+
+//use eventListeners to fetch buttons user clicks
+
 
 //add function
 function addNum(firstNum, secNum){
@@ -26,6 +34,11 @@ console.log(multiplyNum(9, 3))
 console.log(addNum(9, 3))
 console.log(subtractNum(9, 3))
 console.log(operate("*", 7, 2))
+console.log(numbers)
+console.log(display)
+console.log(clear)
+console.log(calculate)
+console.log(operNumbers)
 
 
 //Create a new function operate that
@@ -42,3 +55,5 @@ function operate (operator, firstNum, secNum){
         return console.log(divideNum(firstNum, secNum))
     }
 }
+
+
