0e4db960a23fc1c12f388b463be044dbc478b6e3
Samuel Onyebekeh <deemedx0@gmail.com>
2025-10-16 14:32:33 +0100

    added basic calculation for one digits

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //global variables for important inputs
-let firstNum;
-let secNum;
-let operator;
+let firstNum = null
+let secNum = null
+let operator = null
 let numbers = document.getElementsByClassName("buttons");
 let operNumbers = document.getElementsByClassName("oper-buttons")
 let calculate = document.getElementById("equalbutton")
@@ -9,8 +9,37 @@ let clear = document.getElementById("clear-button")
 display = document.getElementById("screen-button")
 
 //use eventListeners to fetch buttons user clicks
+//for numbers
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function() {
+    const clickTextContent = numbers[i].textContent;
+    display.textContent = clickTextContent;
 
+    if (firstNum === null && operator === null) {
+      firstNum = Number(clickTextContent);
+    }
+    if (firstNum !== null && operator !== null) {
+      secNum = Number(clickTextContent);
+      
+    }
+  })
+}
+//for operator
+ for (let i = 0; i < operNumbers.length; i++)
+    operNumbers[i].addEventListener("click", function(){
+     const clickedOper = operNumbers[i].textContent;
+       display.textContent = clickedOper;
 
+       operator = clickedOper
+       return operator
+    })
+
+//for equalsbutton
+calculate.addEventListener("click", operate)
+
+//for clearAll
+clear.addEventListener("click", clearAll)
+ 
 //add function
 function addNum(firstNum, secNum){
     return firstNum + secNum
@@ -28,32 +57,29 @@ function divideNum(firstNum, secNum){
     return firstNum / secNum
 }
 
-//test using console
-console.log(divideNum(9, 3))
-console.log(multiplyNum(9, 3))
-console.log(addNum(9, 3))
-console.log(subtractNum(9, 3))
-console.log(operate("*", 7, 2))
-console.log(numbers)
-console.log(display)
-console.log(clear)
-console.log(calculate)
-console.log(operNumbers)
-
-
 //Create a new function operate that
 //  takes an operator and two numbers 
 // and then calls one of the above functions on the numbers.
-function operate (operator, firstNum, secNum){
-    if( operator === "+"){
-        return console.log(addNum(firstNum, secNum))
-    }else if( operator === "-"){
-        return console.log(subtractNum(firstNum, secNum))
-    }else if( operator === "*"){
-        return console.log(multiplyNum(firstNum, secNum))
-    }else if( operator === "/"){
-        return console.log(divideNum(firstNum, secNum))
+function operate (){
+    if (operator === "+"){
+        return display.textContent = addNum(firstNum, secNum);
+    } else if (operator === "-"){
+        return display.textContent = subtractNum(firstNum, secNum);
+    } else if (operator === "*"){
+        return display.textContent = multiplyNum(firstNum, secNum);
+    } else if (operator === "/"){
+        return display.textContent = divideNum(firstNum, secNum);
+    } else {
+        return display.textContent = "Sci. Err";
     }
 }
 
+
+//function to clear all
+function clearAll(){
+    firstNum;
+    operator;
+    secNum;
+    display.textContent = "|"
+}
 
